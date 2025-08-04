08054ffc3b7a30cf795529442776c968d0aeba26
AverageApple288 <118760633+AverageApple288@users.noreply.github.com>
2025-08-04 15:16:55 +0100

    Timings for convolve

--- backend/convolve.py
+++ backend/convolve.py
@@ -1,9 +1,16 @@
+import time
+
 import numpy as np
 
 def convolve(image, conv_filter):
+    startTime = time.time()
+    print("Started initialising variables:")
     filter_height, filter_width, in_channels = conv_filter.shape
     img_height, img_width, img_channels = image.shape
+    intitialiseVariables = time.time()
+    print(f"Variables initialised in {intitialiseVariables - startTime} seconds")
 
+    print("Started padding image:")
     pad_h = (filter_height - 1) // 2
     pad_w = (filter_width - 1) // 2
 
@@ -16,9 +23,26 @@ def convolve(image, conv_filter):
 
     output = np.zeros((img_height, img_width, in_channels))
 
+    paddingDone = time.time()
+    print(f"Image padded in {paddingDone - intitialiseVariables} seconds")
+
+
     for c in range(in_channels):
         for i in range(img_height):
             for j in range(img_width):
-                patch = padded_image[i:i+filter_height, j:j+filter_width, c]
-                output[i, j, c] = np.sum(patch * conv_filter[:, :, c])
+                if i == 0 and j == 0:
+                    print("Started patch:")
+                    startPatch = time.time()
+                    patch = padded_image[i:i + filter_height, j:j + filter_width, c]
+                    endPatch = time.time()
+                    print(f"Patch created in {endPatch - startPatch} seconds")
+                    print("Started convolution:")
+                    startConv = time.time()
+                    output[i, j, c] = np.sum(patch * conv_filter[:, :, c])
+                    endConv = time.time()
+                    print(f"Convolution done in {endConv - startConv} seconds")
+                else:
+                    patch = padded_image[i:i + filter_height, j:j + filter_width, c]
+                    output[i, j, c] = np.sum(patch * conv_filter[:, :, c])
+
     return output
